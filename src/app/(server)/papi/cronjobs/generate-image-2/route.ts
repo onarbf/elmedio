22f5acc371b0124c752423def2414da2444073f8
Onarbf <onarbf@gmail.com>
2024-12-18 15:55:29 +0100

    --

--- src/app/(server)/papi/cronjobs/generate-image-2/route.ts
+++ src/app/(server)/papi/cronjobs/generate-image-2/route.ts
@@ -36,7 +36,7 @@ export async function GET() {
     const imageBuffer = base64ToArrayBuffer(image.base64)
     console.log('image', image)
 
-    const uploadsDir = 'tmp/uploads'
+    const uploadsDir = '/tmp'
     if (!fs.existsSync(uploadsDir)) {
       fs.mkdirSync(uploadsDir, { recursive: true }) // Crear el directorio si no existe
     }
