ecbb7f3a1363777cf15bea99c663bc3b2a0aeaf4
Onarbf <onarbf@gmail.com>
2024-12-21 18:42:17 +0100

    --

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,19 +1,19 @@
 const styles = {
   textStyles: {
-    p: "text-normal",
-    h1: "text-5xl font-bold",
-    h2: "text-5xl",
-    h3: "text-3xl",
-    h4: "text-2xl",
-    h5: "text-2xl",
-    h6: "text-base leading-[1rem] tracking-[-0.03rem]",
-    small: "text-sm",
-    superSmall: "text-[0.8rem]",
+    p: 'text-normal',
+    h1: 'md:text-3xl text-5xl font-bold',
+    h2: 'md:text-2xl text-5xl',
+    h3: 'text-3xl',
+    h4: 'text-2xl',
+    h5: 'text-2xl',
+    h6: 'text-base leading-[1rem] tracking-[-0.03rem]',
+    small: 'text-sm',
+    superSmall: 'text-[0.8rem]',
   },
   linkStyles: {
-    default: "text-primary-700 hover:text-primary-900 underline",
-    clean: "",
+    default: 'text-primary-700 hover:text-primary-900 underline',
+    clean: '',
   },
-};
+}
 
-export default styles;
+export default styles
